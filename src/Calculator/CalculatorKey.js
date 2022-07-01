dded3e6628772c3ea2b43d57440d531d2a00d7aa
Serber92 <mustafahosseini691@gmail.com>
2022-07-01 02:31:51 -0700

    Update CalculatorKey.js

--- src/Calculator/CalculatorKey.js
+++ src/Calculator/CalculatorKey.js
@@ -2,11 +2,11 @@ import React from "react";
 import PointTarget from "react-point";
 
 const CalculatorKey = (props) => {
-  const { onPress, className, ...props1 } = props;
+  const { onPress, className, ...rest } = props;
 
   return (
     <PointTarget onPoint={onPress}>
-      <button className={`calculator-key ${className}`} {...props1} />
+      <button className={`calculator-key ${className}`} {...rest} />
     </PointTarget>
   );
 };
